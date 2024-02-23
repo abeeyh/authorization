3f0d2fe6544fb8fefcef138c4075c667e6b84b72
gabriel@zenfisio.com <messina@hotmail.com.br>
2024-02-23 10:36:32 -0300

    fix: corection for linux dist

--- src/module/users/users.module.ts
+++ src/module/users/users.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
-import { TokenService } from 'src/utils/token';
-import { CryptoService } from 'src/utils/crypto';
-import { AuthGuard } from 'src/core/auth/auth.guard';
-import { AuthModule } from 'src/core/auth/auth.module';
+import { TokenService } from '../../utils/token';
+import { CryptoService } from '../../utils/crypto';
+import { AuthGuard } from '../../core/auth/auth.guard';
+import { AuthModule } from '../../core/auth/auth.module';
 
 @Module({
   imports: [AuthModule],
